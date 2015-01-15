c84ebb8eee351cb80886ebbbed39c20672d3ea8d
zhangls <ihrthk@gmail.com>
2015-01-15 09:44:59 +0800

    add holder

--- app/src/main/java/com/zhangls/videoplayer/MyExoplayer.java
+++ app/src/main/java/com/zhangls/videoplayer/MyExoplayer.java
@@ -2,105 +2,62 @@ package com.zhangls.videoplayer;
 
 import android.app.Activity;
 import android.media.MediaCodec;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-import com.google.android.exoplayer.DefaultLoadControl;
 import com.google.android.exoplayer.ExoPlayer;
-import com.google.android.exoplayer.LoadControl;
+import com.google.android.exoplayer.FrameworkSampleSource;
 import com.google.android.exoplayer.MediaCodecVideoTrackRenderer;
 import com.google.android.exoplayer.TrackRenderer;
-import com.google.android.exoplayer.chunk.ChunkSampleSource;
-import com.google.android.exoplayer.chunk.ChunkSource;
-import com.google.android.exoplayer.chunk.FormatEvaluator;
-import com.google.android.exoplayer.dash.DashChunkSource;
-import com.google.android.exoplayer.upstream.BufferPool;
-import com.google.android.exoplayer.upstream.DataSource;
-import com.google.android.exoplayer.upstream.DataSpec;
-
-import java.io.IOException;
 
 
-public class MyExoplayer extends Activity {
+public class MyExoplayer extends Activity implements SurfaceHolder.Callback {
 
     private static final int VIDEO_BUFFER_SEGMENTS = 200;
     private static final int BUFFER_SEGMENT_SIZE = 64 * 1024;
+
+
+    SurfaceView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exoplayer);
 
-        ChunkSource videoChunkSource = new DashChunkSource(new DataSource() {
-            @Override
-            public long open(DataSpec dataSpec) throws IOException {
-                return 0;
-            }
+        videoView = (SurfaceView) findViewById(R.id.video);
+        videoView.getHolder().addCallback(this);
+    }
 
-            @Override
-            public void close() throws IOException {
 
-            }
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
 
-            @Override
-            public int read(byte[] buffer, int offset, int readLength) throws IOException {
-                return 0;
-            }
-        }, new FormatEvaluator.FixedEvaluator());
-        LoadControl loadControl = new DefaultLoadControl(new BufferPool(BUFFER_SEGMENT_SIZE));
-        ChunkSampleSource videoSampleSource = new ChunkSampleSource(videoChunkSource, loadControl,
-                VIDEO_BUFFER_SEGMENTS * BUFFER_SEGMENT_SIZE, true, new Handler(), new ChunkSampleSource.EventListener() {
-            @Override
-            public void onLoadStarted(int sourceId, String formatId, int trigger, boolean isInitialization, int mediaStartTimeMs, int mediaEndTimeMs, long length) {
-
-            }
-
-            @Override
-            public void onLoadCompleted(int sourceId, long bytesLoaded) {
-
-            }
-
-            @Override
-            public void onLoadCanceled(int sourceId, long bytesLoaded) {
-
-            }
-
-            @Override
-            public void onUpstreamDiscarded(int sourceId, int mediaStartTimeMs, int mediaEndTimeMs, long bytesDiscarded) {
-
-            }
-
-            @Override
-            public void onUpstreamError(int sourceId, IOException e) {
-
-            }
-
-            @Override
-            public void onConsumptionError(int sourceId, IOException e) {
-
-            }
-
-            @Override
-            public void onDownstreamDiscarded(int sourceId, int mediaStartTimeMs, int mediaEndTimeMs, long bytesDiscarded) {
-
-            }
-
-            @Override
-            public void onDownstreamFormatChanged(int sourceId, String formatId, int trigger, int mediaTimeMs) {
-
-            }
-        }, 1);
         // 1. Instantiate the player.
         ExoPlayer exoplayer = ExoPlayer.Factory.newInstance(1);
         // 2. Construct renderers.
-        TrackRenderer videoRender = new MediaCodecVideoTrackRenderer(videoSampleSource, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT);
+        String string = "http://mofunsky-video.qiniudn.com/90/193/2014082012541850633.webm";
+//        String string = "http://live.3gv.ifeng.com/zixun.m3u8";
+        FrameworkSampleSource frameworkSampleSource = new FrameworkSampleSource(this, Uri.parse(string), null, 1);
+        TrackRenderer videoRender = new MediaCodecVideoTrackRenderer(frameworkSampleSource, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT);
         // 3. Inject the renderers through prepare
         exoplayer.prepare(videoRender);
         // 4. Pass the surface to the video renderer.
         exoplayer.sendMessage(videoRender, MediaCodecVideoTrackRenderer.MSG_SET_SURFACE,
-                surface);
+                holder.getSurface());
         // 5. Start playback.
         exoplayer.setPlayWhenReady(true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 
 
