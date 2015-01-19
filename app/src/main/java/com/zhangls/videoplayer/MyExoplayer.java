9e4c80018a48a8f4f7921d09c55dcf60a575cc44
zhangls <ihrthk@gmail.com>
2015-01-19 14:22:57 +0800

    add h264 live stream MediaCodecVideoTrackRenderer

--- app/src/main/java/com/zhangls/videoplayer/MyExoplayer.java
+++ app/src/main/java/com/zhangls/videoplayer/MyExoplayer.java
@@ -9,8 +9,24 @@ import android.view.SurfaceView;
 
 import com.google.android.exoplayer.ExoPlayer;
 import com.google.android.exoplayer.FrameworkSampleSource;
+import com.google.android.exoplayer.LoadControl;
 import com.google.android.exoplayer.MediaCodecVideoTrackRenderer;
+import com.google.android.exoplayer.SampleSource;
 import com.google.android.exoplayer.TrackRenderer;
+import com.google.android.exoplayer.chunk.ChunkSampleSource;
+import com.google.android.exoplayer.chunk.ChunkSource;
+import com.google.android.exoplayer.chunk.Format;
+import com.google.android.exoplayer.chunk.FormatEvaluator;
+import com.google.android.exoplayer.dash.DashChunkSource;
+import com.google.android.exoplayer.dash.mpd.Representation;
+import com.google.android.exoplayer.upstream.Allocator;
+import com.google.android.exoplayer.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer.upstream.HttpDataSource;
+import com.google.android.exoplayer.util.MimeTypes;
+
+import org.apache.http.Header;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.auth.BasicScheme;
 
 
 public class MyExoplayer extends Activity implements SurfaceHolder.Callback {
@@ -58,6 +74,52 @@ public class MyExoplayer extends Activity implements SurfaceHolder.Callback {
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
 
+    }
+
+    public MediaCodecVideoTrackRenderer getTrackRenderer() {
+        HttpDataSource videoDataSource = new HttpDataSource("us",
+                HttpDataSource.REJECT_PAYWALL_TYPES, new DefaultBandwidthMeter());
+        Header header = BasicScheme.authenticate(new UsernamePasswordCredentials("admin", "111111"),
+                "UTF-8", false);
+        videoDataSource.setRequestProperty(header.getName(), header.getValue());
+        Format format = new Format("1", MimeTypes.VIDEO_H264, 320, 240, 1, 1, 1);
+        String uriString = "http://192.168.0.110/video/ACVS-H264.cgi?profileid=3";
+        ChunkSource videoChunkSource = new DashChunkSource(videoDataSource,
+                new FormatEvaluator.AdaptiveEvaluator(new DefaultBandwidthMeter()),
+                Representation.SingleSegmentRepresentation.newInstance(0L, -1L, "", -1L, format,
+                        Uri.parse(uriString)
+                        , 0L, 0L, 0L, 0L, -1L));
+
+
+        SampleSource videoSampleSource = new ChunkSampleSource(videoChunkSource,
+                new LoadControl() {
+                    @Override
+                    public void register(Object loader, int bufferSizeContribution) {
+
+                    }
+
+                    @Override
+                    public void unregister(Object loader) {
+
+                    }
+
+                    @Override
+                    public Allocator getAllocator() {
+                        return null;
+                    }
+
+                    @Override
+                    public void trimAllocator() {
+
+                    }
+
+                    @Override
+                    public boolean update(Object loader, long playbackPositionUs, long nextLoadPositionUs, boolean loading, boolean failed) {
+                        return false;
+                    }
+                }, VIDEO_BUFFER_SEGMENTS * BUFFER_SEGMENT_SIZE, true);
+        return new MediaCodecVideoTrackRenderer(
+                videoSampleSource, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT);
     }
 
 
